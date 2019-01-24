d116299270d1a3774cc156f32250cfae21ca5a49
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2019-01-24 15:39:27 -0500

    Added @export to all methods

--- R/asBranchComponent.R
+++ R/asBranchComponent.R
@@ -13,120 +13,139 @@ asBranchComponent <- function(x)
     UseMethod("asBranchComponent")
 }
 
+#' @export
 asBranchComponent.default<-function(x)
 {
     as.matrix(x)
 }
 
 ## adjclust package
+#' @export
 asBranchComponent.chac <- function(x)
 {
     getComponentsfromMerge(x$merge)
 }
 
 ## adpclust package
+#' @export
 asBranchComponent.adpclust <- function(x)
 {
     x$clusters
 }
 
 ## afCEC package
+#' @export
 asBranchComponent.afCEC <- function(x)
 {
     x$labels
 }
 
 ## apcluster package
+#' @export
 asBranchComponent.apcluster <- function(x)
 {
     x
 }
+#' @export
 asBranchComponent.AggExResul <- function(x)
 {
     getComponentsfromMerge(x$merge)
 }
 
 ## bclust package
+#' @export
 asBranchComponent.bclust <- function(x)
 {
     getComponentsfromMerge(x$merge)
 }
 
+#' @export
 asBranchComponent.bclustvs <- function(x)
 {
     getComponentsfromMerge(x$merge)
 }
-## bclust package
 
-## biclust package
+## biclust package  #Nothing needed here
 
 ## package cba
+#' @export
 asBranchComponent.ccfkms <- function(x)
 {
     x$cl
 }
 
 ## package cclust
+#' @export
 asBranchComponent.cclust <- function(x)
 {
     x$cluster
 }
 
 ##  package CEC
+#' @export
 asBranchComponent.cec <- function(x)
 {
     x$cluster
 }
 
-####  package Ckmeans.1d.dp  ####???????????????????????????????????????????????????????????
+####  package Ckmeans.1d.dp  #### TO DO
+#' @export
 asBranchComponent.Ckmeans.1d.dp <- function(x)
 {
     x$cluster
 }
 
 ##  package clues
+#' @export
 asBranchComponent.clues <- function(x)
 {
     x$mem
 }
 
 ## package cluster
+#' @export
 asBranchComponent.agnes <- function(x)
 {
     getComponentsfromMerge(x$merge)
 }
 
+#' @export
 asBranchComponent.clara <- function(x)
 {
     x$clustering
 }
 
+#' @export
 asBranchComponent.diana <- function(x)
 {
     getComponentsfromMerge(x$merge)
 }
 
+#' @export
 asBranchComponent.fanny <- function(x)
 {
     x$clustering
 }
 
+#' @export
 asBranchComponent.pam <- function(x)
 {
     x$clustering
 }
 
-## ClusterR package
+## ClusterR package  TODO
+ 
+## clustMD package   TODO
 
-## clustMD package
-
-## CoClust package
+## CoClust package  TODO
+#' @export
 asBranchComponent.CoClust <- function(x)
 {
-    # don't know how to do this!!!!
+    stop("don't know how to handle result from CoClust package")
 }
 
 ##  compHclust package
+#' @export
 #asBranchComponent.compHclust <- function(x)
 #{
 #}
@@ -135,6 +154,7 @@ asBranchComponent.CoClust <- function(x)
 ## only need default processing
 
 ## contaminatedmixt package
+#' @export
 #asBranchComponent.CNmixt <- function(x)
 #{
 #don't know how to do this!!!
@@ -147,6 +167,7 @@ asBranchComponent.CoClust <- function(x)
 ## package CrossClustering returns list??????????????????????
 
 ## Package 'CRPClustering' only requires default processing ##
+#' @export
 asBranchComponent.dbscan_fast <- function(x)
 {
     tmp <- x$cluster
@@ -161,30 +182,35 @@ asBranchComponent.dbscan_fast <- function(x)
 }
 
 ## Fclust package  ##
+#' @export
 asBranchComponent.fclust <- function(x)
 {
     x$clus
 }
 
 ##  hierarchical based clustering
+#' @export
 asBranchComponent.hclust <- function(x)
 {
     getComponentsfromMerge(x$merge)
 }
 
 ## package kernlab
+#' @export
 asBranchComponent.specc <- function(x)
 {
     x@.Data
 }
 
 ## kmeans package
+#' @export
 asBranchComponent.kmeans <- function(x)
 {
     x$cluster
 }
 
 ##  mixture based clustering
+#' @export
 asBranchComponent.Mclust <- function(x)
 {
     x$classification
